9a78e8979dcb88754f12a6a86040f1f1eeb70b9a
CodeByAhmad <devdroid47@gmail.com>
2025-01-18 14:37:08 +0500

    Added Counter

--- App.js
+++ App.js
@@ -1,36 +1,95 @@
-import {Image, SafeAreaView, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {
+  Alert,
+  Button,
+  Image,
+  Pressable,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {React, useState} from 'react';
 
 const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  // const countIncrement = () => {
+  //   setCounter(counter + 1);
+  // };
+  // const countDecrement = () => {
+  //   setCounter(counter - 1);
+  // };
+
+  const countIncreDecre = (value, action) => {
+    if (action == 'plus') {
+      setCounter(counter + 1);
+    } else {
+      setCounter(counter - 1);
+    }
+  };
+
   return (
     <View
       style={{
         flex: 1,
-        backgroundColor: 'gray',
+        backgroundColor: 'orange',
+        justifyContent: 'center',
       }}>
-      <SafeAreaView style={{flex: 1, backgroundColor: 'red'}}>
-        <View
-          style={{
-            flex: 0.5,
-            backgroundColor: 'green',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>
-          <Image
-            style={{height: 200, width: 200, borderRadius: 100}}
-            source={require('./src/assets/images/car.jpg')}
-          />
+      <SafeAreaView>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View style={{alignItems: 'center'}}>
+            <TouchableOpacity
+              activeOpacity={0.7}
+              onPress={() => countIncreDecre(counter, 'plus')}
+              style={{
+                backgroundColor: 'green',
+                height: 50,
+                width: 50,
+                borderRadius: 25,
+                alignItems: 'center',
+                justifyContent: 'center',
+                paddingHorizontal: 20,
+              }}>
+              <Text style={{color: 'white'}}>+</Text>
+            </TouchableOpacity>
+            <View style={{alignItems: 'center', marginVertical: 10}}>
+              <Text style={{fontSize: 30}}>{counter}</Text>
+            </View>
 
-          <Text style={{color: 'white', fontWeight: 'bold', fontSize: 24}}>
-            Code Builder
-          </Text>
-        </View>
-        <View
-          style={{
-            flex: 0.5,
-            backgroundColor: 'orange',
-            alignItems: 'center',
-          }}></View>
+            <TouchableOpacity
+              activeOpacity={0.7}
+              onPress={() => countIncreDecre(counter, 'minus')}
+              style={{
+                backgroundColor: 'green',
+                height: 50,
+                width: 50,
+                borderRadius: 25,
+
+                borderRadius: 25,
+                alignItems: 'center',
+                justifyContent: 'center',
+                paddingHorizontal: 20,
+              }}>
+              <Text style={{color: 'white', fontSize: 25}}>-</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              activeOpacity={0.7}
+              onPress={() => setCounter(0)}
+              style={{
+                backgroundColor: 'green',
+                height: 40,
+                width: 100,
+                alignItems: 'center',
+                justifyContent: 'center',
+                paddingHorizontal: 20,
+                marginTop: 20,
+              }}>
+              <Text style={{color: 'white', fontSize: 14}}>Reset</Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
       </SafeAreaView>
     </View>
   );
